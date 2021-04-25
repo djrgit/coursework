aec6afff72d673477b53d5b3d4be766c11967dd1
dj <yearntolearn@outlook.com>
2021-04-25 13:45:54 -0400

    Error Handling: Days 1-3

---
+++ talkpython/100-days-of-code-in-python/my_progress/day_025_027_error_handling/day_027_gold_mining_boe_error_handling.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Back of envelope calculations for a precious metals mining company
+
+
+try:
+    oz_per_yr = int(input("How many ounces will be produced annually?: "))
+    price_per_oz = int(input("What is the estimated USD price per ounce for the year?: "))
+    aisc = int(input("What is the USD AISC?: "))
+    exchange_rate = float(input("What is the USD/CAD exchange rate?: "))
+    shrs_out = int(input("How many shares are outstanding?: "))
+    pe = int(input("What is the estimated PE ratio?: "))
+
+    values = {
+        'oz_per_yr':     oz_per_yr,
+        'price_per_oz':  price_per_oz,
+        'aisc':          aisc,
+        'exchange_rate': exchange_rate,
+        'shrs_out':      shrs_out,
+        'pe':            pe
+    }
+
+
+    def share_price_calc(vals):
+
+        oz_per_yr     = vals['oz_per_yr']
+        price_per_oz  = vals['price_per_oz']
+        aisc          = vals['aisc']
+        exchange_rate = vals['exchange_rate']
+        shrs_out      = vals['shrs_out']
+        pe            = vals['pe']
+
+        share_price = (((oz_per_yr * (price_per_oz - aisc)) * exchange_rate) /shrs_out) * pe
+
+        return share_price
+
+except ValueError:
+    print("ValueError:  Please enter a number (not a string).")
+
+finally:
+
+    try:
+        print(share_price_calc(values))
+
+    except Exception:
+        print("Exception resulting from ValueError:  Please enter a number (not a string).")
